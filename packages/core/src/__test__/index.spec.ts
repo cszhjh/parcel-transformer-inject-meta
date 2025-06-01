75ea7ab9a95a937ab5b9e782afe75f3b47a0e3d1
Zhong <cszhjh@gmail.com>
2025-06-01 13:45:42 +0800

    test: add case

--- packages/core/src/__test__/index.spec.ts
+++ packages/core/src/__test__/index.spec.ts
@@ -16,56 +16,91 @@ const template = `
 
 const config: MetaConfig = {
   content: {
+    lang: 'en',
     title: 'title',
     description: 'description',
-    lang: 'en',
-  },
-  entries: {
-    'src/index.html': {
-      builtinMeta: {
-        viewport: false,
-      },
-      content: {
-        title: 'index title',
-        description: 'index description',
-        lang: 'zh-CN',
-        metas: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
-      },
-    },
-    'src/en.html': {
-      content: {
-        title: 'en title',
-        description: 'en description',
-        lang: 'en',
-      },
-    },
+    keywords: 'keywords',
   },
 }
 
 test('basic usage', () => {
-  expect(
-    transform({
-      code: template,
-      projectRoot: '.',
-      config,
-      filePath: 'src/index.html',
-    }),
-  ).toMatchInlineSnapshot(`
+  const options = {
+    code: template,
+    projectRoot: '.',
+    config,
+    filePath: 'src/index.html',
+  }
+
+  expect(transform(options)).toMatchInlineSnapshot(`
     "
     <!doctype html>
-    <html lang="zh-CN">
+    <html lang="en">
       <head>
     		<meta charset="UTF-8" />
+    		<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no,viewport-fit=cover" />
     		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
     		<meta name="google" content="notranslate" />
     		<meta name="format-detection" content="telephone=no" />
-    		<meta property="og:title" content="index title" />
-    		<meta property="og:description" content="index description" />
-    		<meta name="viewport" content="width=device-width, initial-scale=1" />
-    		<meta name="description" content="index description" />
-    		<meta name="keywords" content="" />
+    		<meta property="og:title" content="title" />
+    		<meta property="og:description" content="description" />
+    		<meta name="description" content="description" />
+    		<meta name="keywords" content="keywords" />
 
-        <title>index title</title>
+        <title>title</title>
+      </head>
+      <body>
+        <h1>Parcel Vanilla App</h1>
+      </body>
+    </html>
+    "
+  `)
+})
+
+test('entries', () => {
+  const options = {
+    code: template,
+    projectRoot: '.',
+    config: {
+      ...config,
+      entries: {
+        'src/*.html': {
+          content: {
+            lang: 'en',
+            title: 'Test Title',
+            description: 'Test Description',
+            keywords: 'Test, Keywords',
+          },
+        },
+        'src/index.html': {
+          content: {
+            title: 'Index Title',
+            og: {
+              url: 'https://example.com',
+            },
+          },
+        },
+      },
+    },
+    filePath: 'src/index.html',
+  }
+
+  expect(transform(options)).toMatchInlineSnapshot(`
+    "
+    <!doctype html>
+    <html lang="en">
+      <head>
+    		<meta charset="UTF-8" />
+    		<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no,viewport-fit=cover" />
+    		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    		<meta name="google" content="notranslate" />
+    		<meta name="format-detection" content="telephone=no" />
+    		<meta property="og:title" content="Index Title" />
+    		<meta property="og:description" content="Test Description" />
+    		<meta property="og:url" content="https://example.com" />
+    		<meta name="description" content="Test Description" />
+    		<meta name="keywords" content="Test, Keywords" />
+
+        <title>Index Title</title>
       </head>
       <body>
         <h1>Parcel Vanilla App</h1>
