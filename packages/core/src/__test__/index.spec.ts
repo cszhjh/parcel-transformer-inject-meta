8881ca61f683b3b873fe16a30f9e181234b15e0f
Zhong <cszhjh@gmail.com>
2025-06-01 13:36:51 +0800

    fix: fix error in merging entries configuration

--- packages/core/src/__test__/index.spec.ts
+++ packages/core/src/__test__/index.spec.ts
@@ -1,5 +1,6 @@
 import { expect, test } from 'vitest'
 import { transform } from '../transform'
+import { MetaConfig } from '../types'
 
 const template = `
 <!doctype html>
@@ -13,11 +14,31 @@ const template = `
 </html>
 `
 
-const config = {
+const config: MetaConfig = {
   content: {
     title: 'title',
     description: 'description',
     lang: 'en',
+  },
+  entries: {
+    'src/index.html': {
+      builtinMeta: {
+        viewport: false,
+      },
+      content: {
+        title: 'index title',
+        description: 'index description',
+        lang: 'zh-CN',
+        metas: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+      },
+    },
+    'src/en.html': {
+      content: {
+        title: 'en title',
+        description: 'en description',
+        lang: 'en',
+      },
+    },
   },
 }
 
@@ -32,19 +53,19 @@ test('basic usage', () => {
   ).toMatchInlineSnapshot(`
     "
     <!doctype html>
-    <html lang="en">
+    <html lang="zh-CN">
       <head>
     		<meta charset="UTF-8" />
-    		<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no,viewport-fit=cover" />
     		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
     		<meta name="google" content="notranslate" />
     		<meta name="format-detection" content="telephone=no" />
-    		<meta property="og:title" content="title" />
-    		<meta property="og:description" content="description" />
-    		<meta name="description" content="description" />
+    		<meta property="og:title" content="index title" />
+    		<meta property="og:description" content="index description" />
+    		<meta name="viewport" content="width=device-width, initial-scale=1" />
+    		<meta name="description" content="index description" />
     		<meta name="keywords" content="" />
 
-        <title>title</title>
+        <title>index title</title>
       </head>
       <body>
         <h1>Parcel Vanilla App</h1>
--- packages/core/src/transform.ts
+++ packages/core/src/transform.ts
@@ -39,7 +39,7 @@ export function getMetaConfig(options: Options) {
       },
       content,
     },
-    entryConfigs,
+    ...entryConfigs,
   )
 }
 
